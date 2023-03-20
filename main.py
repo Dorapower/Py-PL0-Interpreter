ec5743ea1faac85b63f19f581b97ceed84d69371
Dorapower <thatisluo@outlook.com>
2023-03-20 14:54:06 +0800

    feat: implement parser class

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 # This file implements a parser for PL/0 source code.
+from __future__ import annotations
 
 from enum import IntEnum
 from enum import StrEnum
 from typing import List, NamedTuple
 import string
-
-from __future__ import annotations
 
 
 class TokenType(IntEnum):
@@ -51,7 +50,7 @@ class Token:
         return Token(TokenType.Number, number)
 
     @staticmethod
-    def name(name: str):
+    def name(name: str = ''):
         return Token(TokenType.Name, name)
 
     @staticmethod
@@ -136,6 +135,10 @@ class Lexer:
 
         # Invalid token
         raise SyntaxError('Invalid token')
+
+    @property
+    def i(self):
+        return self._i
 
 
 # TODO: Separate AST into its own file
@@ -272,6 +275,260 @@ class Program(NamedTuple):
     block: Block
 
 
+class Parser:
+    lx: Lexer
+
+    def __init__(self, lx: Lexer):
+        self.lx = lx
+
+    def check(self, token: Token) -> bool:
+        """
+        Checks if the next token is the given token, and if so, consumes it
+        """
+        cur = self.lx.i
+        next_token = self.lx.next()
+        if next_token == token:
+            return True
+        # if the next_token is a name, we dont need to check the value
+        if next_token.type == TokenType.Name and token.type == TokenType.Name:
+            return True
+        self.lx._i = cur
+        return False
+
+    def program(self) -> Program:
+        """
+        Parses a program
+        """
+        block = self.block()
+        if not self.check(Token.op('.')):
+            raise SyntaxError('Expected "."')
+        return Program(block)
+
+    def block(self) -> Block:
+        """
+        Parses a block
+        """
+        consts = []
+        variables = []
+        procs = []
+        if self.check(Token.keyword('const')):
+            consts = self.consts()
+        if self.check(Token.keyword('var')):
+            variables = self.vars()
+        while self.check(Token.keyword('procedure')):
+            procs.append(self.procedure())
+        stmt = self.statement()
+        return Block(consts, variables, procs, stmt)
+
+    def consts(self) -> List[Const]:
+        """
+        Parses a list of constants
+        """
+        consts = []
+        while True:
+            name = self.name()
+            if not self.check(Token.op('=')):
+                raise SyntaxError('Expected "="')
+            value = self.number()
+            consts.append(Const(name, value))
+            if not self.check(Token.op(',')):
+                break
+        if not self.check(Token.op(';')):
+            raise SyntaxError('Expected ";"')
+        return consts
+
+    def vars(self) -> List[Var]:
+        """
+        Parses a list of variables
+        """
+        variables = []
+        while True:
+            name = self.name()
+            variables.append(Var(name))
+            if not self.check(Token.op(',')):
+                break
+        if not self.check(Token.op(';')):
+            raise SyntaxError('Expected ";"')
+        return variables
+
+    def name(self) -> str:
+        """
+        Parses a name
+        """
+        ident = self.lx.next()
+        if ident.type != TokenType.Name:
+            raise SyntaxError('Expected name')
+        return ident.value
+
+    def number(self) -> int:
+        """
+        Parses a number
+        """
+        num = self.lx.next()
+        if num.type != TokenType.Number:
+            raise SyntaxError('Expected number')
+        return num.value
+
+    def name_or_number(self) -> str | int:
+        """
+        Parses a name or number
+        """
+        ident = self.lx.next()
+        if ident.type == TokenType.Name:
+            return ident.value
+        elif ident.type == TokenType.Number:
+            return ident.value
+        raise SyntaxError('Expected name or number')
+
+    def procedure(self) -> Procedure:
+        """
+        Parses a procedure
+        """
+        ident = self.name()
+        if not self.check(Token.op(';')):
+            raise SyntaxError('Expected ";"')
+        block = self.block()
+        if not self.check(Token.op(';')):
+            raise SyntaxError('Expected ";"')
+        return Procedure(ident, block)
+
+    def statement(self) -> Statement:
+        """
+        Parses a statement
+        """
+        if self.check(Token.keyword('begin')):
+            return Statement(self.begin())
+        elif self.check(Token.keyword('if')):
+            return Statement(self.if_statement())
+        elif self.check(Token.keyword('while')):
+            return Statement(self.while_statement())
+        elif self.check(Token.keyword('call')):
+            return Statement(self.call())
+        elif self.check(Token.keyword('odd')):
+            return Statement(self.odd())
+        else:
+            return Statement(self.assignment())
+
+    def begin(self) -> Begin:
+        """
+        Parses a begin statement
+        """
+        body = []
+        while not self.check(Token.keyword('end')):
+            body.append(self.statement())
+            if not self.check(Token.op(';')):
+                raise SyntaxError('Expected ";"')
+        return Begin(body)
+
+    def if_statement(self) -> If:
+        """
+        Parses an if statement
+        """
+        cond = self.condition()
+        if not self.check(Token.keyword('then')):
+            raise SyntaxError('Expected "then"')
+        stmt = self.statement()
+        return If(cond, stmt)
+
+    def while_statement(self) -> While:
+        """
+        Parses a while statement
+        """
+        cond = self.condition()
+        if not self.check(Token.keyword('do')):
+            raise SyntaxError('Expected "do"')
+        stmt = self.statement()
+        return While(cond, stmt)
+
+    def condition(self) -> Condition:
+        """
+        Parses a condition
+        """
+        if self.check(Token.keyword('odd')):
+            return Condition(self.odd())
+        else:
+            return Condition(self.comparison())
+
+    def odd(self) -> OddCondition:
+        """
+        Parses an odd statement
+        """
+        expr = self.expression()
+        return OddCondition(expr)
+
+    def comparison(self) -> ComparisonCondition:
+        """
+        Parses a comparison
+        """
+        left = self.expression()
+        op = self.lx.next()
+        if op.type != TokenType.Op:
+            raise SyntaxError('Expected operator')
+        right = self.expression()
+        return ComparisonCondition(op.value, left, right)
+
+    def call(self) -> Call:
+        """
+        Parses a call statement
+        """
+        ident = self.name()
+        return Call(ident)
+
+    def assignment(self) -> Assignment:
+        """
+        Parses an assignment statement
+        """
+        ident = self.name()
+        if not self.check(Token.op(':=')):
+            raise SyntaxError('Expected ":="')
+        expr = self.expression()
+        return Assignment(ident, expr)
+
+    def expression(self) -> Expression:
+        """
+        Parses an expression
+        """
+        terms, ops = [], []
+        terms.append(self.term())
+        while True:
+            if self.check(Token.op('+')):
+                ops.append('+')
+            elif self.check(Token.op('-')):
+                ops.append('-')
+            else:
+                break
+            terms.append(self.term())
+        return Expression(terms, ops)
+
+    def term(self) -> Term:
+        """
+        Parses a term
+        """
+        factors, ops = [], []
+        factors.append(self.factor())
+        while True:
+            if self.check(Token.op('*')):
+                ops.append('*')
+            elif self.check(Token.op('/')):
+                ops.append('/')
+            else:
+                break
+            factors.append(self.factor())
+        return Term(factors, ops)
+
+    def factor(self) -> Factor:
+        """
+        Parses a factor
+        """
+        if self.check(Token.op('(')):
+            expr = self.expression()
+            if not self.check(Token.op(')')):
+                raise SyntaxError('Expected ")"')
+            return Factor(expr)
+        else:
+            return Factor(self.name_or_number())
+
+
 def test_lexer():
     test_program = \
         """
@@ -290,5 +547,22 @@ def test_lexer():
         print(lexer.next())
 
 
+def test_parser():
+    test_program = \
+        """
+    var i, s;
+    begin
+    i := 0; s := 0;
+    while i < 5 do
+    begin
+        i := i + 1;
+        s := s + i * i
+    end
+    end.
+    """
+    parser = Parser(Lexer(test_program))
+    print(parser.program())
+
+
 if __name__ == '__main__':
-    test_lexer()
+    test_parser()
