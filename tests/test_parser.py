cfa2691061252deacf974974537bbca8884a7c88
Dorapower <thatisluo@outlook.com>
2023-03-23 00:03:30 +0800

    feat: implement some proper test

---
+++ tests/test_parser.py
@@ -0,0 +1,16 @@
+import pytest
+from typing import TypeAlias
+
+from context import parser as mod
+
+Parser: TypeAlias = mod.Parser
+
+
+def test_simple():
+    p = Parser(mod.Lexer('a := - 1 + 2.'))
+    assert p.parse() == mod.Program(mod.Block(consts=[], vars=[], procs=[], stmt=mod.Statement(
+        mod.Assignment('a', mod.Expression(prefix='-', terms=[
+            mod.Term(factors=[mod.Factor(value=1)], ops=[]),
+            mod.Term(factors=[mod.Factor(value=2)], ops=[]),
+        ], ops=['+']))
+    )))
