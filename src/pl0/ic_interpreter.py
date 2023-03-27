530b012404ff7dbe43f4a13a935ef46f2abcec10
Dorapower <thatisluo@outlook.com>
2023-03-27 19:55:09 +0800

    fix: fix a critical issue of arbitrary code execution
    now checking identifier type when trying to call it

--- src/pl0/ic_interpreter.py
+++ src/pl0/ic_interpreter.py
@@ -102,9 +102,12 @@ class ICInterpreter:
                 case IROp.LIT:
                     self.stack.append(ir.opr)
                 case IROp.CALL:
+                    ident = self.retrieve(ir.opr)
+                    if ident.type != symboltable.SymbolType.PROC:
+                        raise Exception(f"Trying to call a non-procedure {ir.opr}")
                     self.stack.append(self.pc)
                     self.env.append(symboltable.SymbolTable())
-                    self.pc = self.retrieve(ir.opr).value
+                    self.pc = ident.value
                 case IROp.RET:
                     self.env.pop()
                     self.pc = self.stack.pop()
